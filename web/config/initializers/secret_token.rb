31a08472f141cae677994fcce04f228180b6cd62
Artmobile <artmobile@gmail.com>
2012-11-05 15:58:38 +0200

    Initialized

---
+++ web/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Web::Application.config.secret_token = '3914e90e6c76e01bc9338f7b77a533d557b1095d66bd28c5d5bc8204c109dc3303f91c02898d0f84153ad05ac93704f9866985a01957a67abf4c5b704592df17'
